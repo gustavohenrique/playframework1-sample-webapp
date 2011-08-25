7ddc9e5dcf72862d7119dde8a0c4061fdcbebfed
Gustavo Henrique <gustavo@gustavohenrique.net>
2011-08-25 00:18:48 -0300

    added extjs again

--- public/js/ie.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.button').mousedown(function(){
-        $(this).addClass('active');
-    }).bind('mouseup mouseout', function(){
-        $(this).removeClass('active');
-    });
-});
