cc4015a75004df44819cd1b399f83af58cf8176d
Gustavo Henrique <gustavo@gustavohenrique.net>
2012-01-21 19:53:35 -0200

    deleted extjs

--- public/js/app/model/Account.js
+++
@@ -1,4 +0,0 @@
-Ext.define('PoupaNiquel.model.Account', {
-    extend: 'Ext.data.Model',
-    fields: ['id', 'name','number','initial'],
-});
--- public/js/app/model/Generic.js
+++
@@ -1,4 +0,0 @@
-Ext.define('PoupaNiquel.model.Generic', {
-    extend: 'Ext.data.Model',
-    fields: ['id', 'name']
-});
